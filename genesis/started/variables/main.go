9b602fc3a206be7b91139fe2841df81bd010cbc6
Deo_Mugabe <79472484+DegraBest@users.noreply.github.com>
2024-04-20 15:12:44 -0500

    Initial commit

---
+++ genesis/started/variables/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+//constant
+const LoginToken string = "dffeagad" /// the name having the capital letter means its public
+
+func main() {
+	var username string = "Jesus"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username) // %T is a place holder
+
+	var isLoggedin bool = true
+	fmt.Println(isLoggedin)
+	fmt.Printf("varriable is of type: %T \n", isLoggedin)
+
+	var smallval uint8 = 255
+	fmt.Println(smallval)
+	fmt.Printf("varriable is of type: %T \n", smallval)
+
+	var anotherVal int
+	fmt.Println(anotherVal)
+	fmt.Printf("variable is of type: %T \n", anotherVal)
+
+	var anotherVal2 string
+	fmt.Println(anotherVal2)
+	fmt.Printf("variable is of type: %T \n", anotherVal2)
+
+	var website = "Jesus is Lord" // by default lexer will make it string base on ur data
+	fmt.Println(website)
+	//no var sytle
+	numberOfUser := 43434 /// := can not be used ouside the function
+	fmt.Println(numberOfUser)
+}
