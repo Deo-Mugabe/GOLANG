effdcb8939b2c2e64dc3e68fe9f9a4872b24b366
Deo_Mugabe <79472484+DegraBest@users.noreply.github.com>
2024-05-17 20:57:48 -0500

    go-ecommerce

--- RestAPi/sample/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type User struct {
-	Id    int    `json:"id" gorm:"primary_key"`
-	Name  string `json:"name" validation:"required"`
-	Email string `json:"email" validation:"required"`
-}
